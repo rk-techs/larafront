81399f4a1d0001af47284ca16f22f2b1c2669efe
Ryosuke.K <ryskdevelop@gmail.com>
2023-06-19 22:59:05 +0900

    refactor: remove welcome, add home page, and create layout template with Vite and Sass

---
+++ src/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title')
+HOME | {{ config('app.name') }}
+@endsection
+
+@section('content')
+<main>
+    メイン
+</main>
+@endsection
---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    {{-- Styles --}}
+    @vite('resources/sass/style.scss')
+</head>
+<body>
+
+    @include('components.header')
+
+    @yield('content')
+
+    @include('components.footer')
+
+</body>
+</html>
