c642f3093ec8df24affae2c397565b6e54bca730
Ryosuke.K <ryskdevelop@gmail.com>
2023-06-20 22:45:45 +0900

    feat: add links to each template views on home page

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -7,7 +7,17 @@ HOME | {{ config('app.name') }}
 @section('content')
 <main class="page-main">
     <div class="container main-container">
-        メイン
+
+        <ul class="list">
+            <li class="list-item"><a href="alert" class="link">alert</a></li>
+            <li class="list-item"><a href="button" class="link">button</a></li>
+            <li class="list-item"><a href="form" class="link">form</a></li>
+            <li class="list-item"><a href="headding" class="link">headding</a></li>
+            <li class="list-item"><a href="list" class="link">list</a></li>
+            <li class="list-item"><a href="icon" class="link">icon</a></li>
+            <li class="list-item"><a href="table" class="link">table</a></li>
+        </ul>
+
     </div>
 </main>
 @endsection
