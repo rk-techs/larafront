15936282975771d118a9df52d1688c766fb136ce
Ryosuke.K <ryskdevelop@gmail.com>
2023-06-21 00:20:42 +0900

    feat: update header

--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -1,5 +1,20 @@
 <header class="page-header">
     <div class="container header-container">
-        ヘッダー
+        <div class="header-title">
+            <a href="/" class="link">larafront</a>
+        </div>
+        <div class="header-nav">
+            <div class="nav-item"><a href="" class="nav-link">メニュー1</a></div>
+            <div class="nav-item"><a href="" class="nav-link">メニュー2</a></div>
+            <div class="nav-item"><a href="" class="nav-link">メニュー3</a></div>
+            <div class="nav-item"><a href="" class="nav-link">メニュー4</a></div>
+            <div class="nav-item"><a href="" class="nav-link">メニュー5</a></div>
+        </div>
+        <div class="header-btn btn btn-create">ログイン</div>
+        <div class="btn mobile-menu">
+            <span class="material-symbols-outlined">
+                menu
+            </span>
+        </div>
     </div>
 </header>
