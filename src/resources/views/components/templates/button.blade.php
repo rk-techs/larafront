c9443576625e472778abf4caa0ced67c617dd40b
Ryosuke.K <ryskdevelop@gmail.com>
2023-06-21 00:20:31 +0900

    feat: update buttons

--- src/resources/views/components/templates/button.blade.php
+++ src/resources/views/components/templates/button.blade.php
@@ -7,17 +7,20 @@
 @section('content')
     <main class="page-main">
         <div class="container main-container">
-            <div class="btn btn-create">登録</div>
-            <div class="btn btn-store">保存</div>
-            <div class="btn btn-edit">編集</div>
-            <div class="btn btn-update">更新</div>
-            <div class="btn btn-delete">削除</div>
-            <div class="btn btn-search">検索</div>
-            <div class="btn btn-submit">送信</div>
-            <div class="btn btn-info">info</div>
-            <div class="btn btn-index">index</div>
-            <div class="btn btn-back">戻る</div>
-            <div class="btn btn-cancel">キャンセル</div>
+
+            <div class="btn-group">
+                <div class="btn btn-create">登録</div>
+                <div class="btn btn-store">保存</div>
+                <div class="btn btn-edit">編集</div>
+                <div class="btn btn-update">更新</div>
+                <div class="btn btn-delete">削除</div>
+                <div class="btn btn-search">検索</div>
+                <div class="btn btn-submit">送信</div>
+                <div class="btn btn-info">info</div>
+                <div class="btn btn-index">index</div>
+                <div class="btn btn-back">戻る</div>
+                <div class="btn btn-cancel">キャンセル</div>
+            </div>
         </div>
     </main>
 @endsection
