ca0269175bb99f4db1d1959d66c1b932ebbdf40b
Ryosuke.K <ryskdevelop@gmail.com>
2023-06-23 07:31:27 +0900

    style: apply formatting

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -4,15 +4,15 @@ const headerToggleButton = document.getElementById('headerToggleButton');
 const headerToggleSymbol = document.getElementById('headerToggleSymbol');
 const collapseNav = document.getElementById('collapseNav');
 
-headerToggleButton.addEventListener('click', function() {
-    collapseNav.classList.toggle('visible');
+headerToggleButton.addEventListener('click', function () {
+  collapseNav.classList.toggle('visible');
 
 
-    if (headerToggleSymbol.textContent == 'menu') {
-        headerToggleSymbol.textContent = 'close';
-    } else {
-        headerToggleSymbol.textContent = 'menu';
-    }
+  if (headerToggleSymbol.textContent == 'menu') {
+    headerToggleSymbol.textContent = 'close';
+  } else {
+    headerToggleSymbol.textContent = 'menu';
+  }
 });
 
 
