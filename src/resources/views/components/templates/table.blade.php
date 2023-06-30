6493d0a6cf7282db1b1f6dd4013a912285d147a8
Ryosuke.K <ryskdevelop@gmail.com>
2023-07-01 00:11:20 +0900

    refactor: table

--- src/resources/views/components/templates/table.blade.php
+++ src/resources/views/components/templates/table.blade.php
@@ -63,23 +63,21 @@
             <table class="table">
                 <thead class="table-header">
                     <tr class="table-row">
-                        <th class="table-cell header-cell">項目1</th>
-                        <th class="table-cell header-cell">項目2</th>
-                        <th class="table-cell header-cell">項目3</th>
-                        <th class="table-cell header-cell">項目4</th>
+                        <th class="th-cell">項目1</th>
+                        <th class="th-cell">項目2</th>
+                        <th class="th-cell">項目3</th>
+                        <th class="th-cell">項目4</th>
                     </tr>
                 </thead>
                 <tbody class="table-body">
-
                     @for ($i = 0; $i < 15; $i++)
                     <tr class="table-row">
-                        <td class="table-cell">データ</td>
-                        <td class="table-cell">データ</td>
-                        <td class="table-cell">データ</td>
-                        <td class="table-cell">データ</td>
+                        <td class="td-cell">データ</td>
+                        <td class="td-cell">データ</td>
+                        <td class="td-cell">データ</td>
+                        <td class="td-cell">データ</td>
                     </tr>
                     @endfor
-
                 </tbody>
             </table>
 
