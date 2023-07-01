2bbd5d7354b1455d4e514eef8f4b6b1fbccb4542
Ryosuke.K <ryskdevelop@gmail.com>
2023-07-01 20:57:58 +0900

    refactor: form

--- src/resources/views/components/templates/form.blade.php
+++ src/resources/views/components/templates/form.blade.php
@@ -16,17 +16,17 @@
                 <div class="form-group">
                     <label class="form-label" for="text-input">Text Input: <span class="required-dot">*</span></label>
                     <input type="text" id="text-input" class="input-field" required>
-                    <div id="text-input-validation-msg" class="validation-msg">This field is required</div>
+                    <div class="validation-msg">This field is required</div>
                 </div>
                 <div class="form-group">
                     <label class="form-label" for="email-input">Email Input: <span class="required-dot">*</span></label>
                     <input type="email" id="email-input" class="input-field" required>
-                    <div id="email-input-validation-msg" class="validation-msg">Please enter a valid email</div>
+                    <div class="validation-msg">Please enter a valid email</div>
                 </div>
                 <div class="form-group">
                     <label class="form-label" for="password-input">Password Input: <span class="required-dot">*</span></label>
                     <input type="password" id="password-input" class="input-field" required>
-                    <div id="password-input-validation-msg" class="validation-msg">Password must be at least 8 characters
+                    <div class="validation-msg">Password must be at least 8 characters
                     </div>
                 </div>
 
@@ -34,14 +34,14 @@
                 <div class="form-group">
                     <label class="form-label" for="date-input">Date Input: <span class="required-dot">*</span></label>
                     <input type="date" id="date-input" class="input-field" required>
-                    <div id="date-input-validation-msg" class="validation-msg">Please select a date</div>
+                    <div class="validation-msg">Please select a date</div>
                 </div>
 
                 <!-- Time Input -->
                 <div class="form-group">
                     <label class="form-label" for="time-input">Time Input: <span class="required-dot">*</span></label>
                     <input type="time" id="time-input" class="input-field" required>
-                    <div id="time-input-validation-msg" class="validation-msg">Please select a time</div>
+                    <div class="validation-msg">Please select a time</div>
                 </div>
 
                 <!-- Checkbox Input -->
@@ -57,7 +57,7 @@
                     <label>
                         <input type="radio" name="radio-input" class="input-field" required> Option 2
                     </label>
-                    <div id="radio-input-validation-msg" class="validation-msg">Please select an option</div>
+                    <div class="validation-msg">Please select an option</div>
                 </div>
 
                 <!-- Select Box -->
@@ -68,12 +68,9 @@
                         <option value="option1">Option 1</option>
                         <option value="option2">Option 2</option>
                     </select>
-                    <div id="select-input-validation-msg" class="validation-msg">Please select an option</div>
+                    <div class="validation-msg">Please select an option</div>
                 </div>
 
-                <div class="form-group">
-                    <button type="submit" class="form-btn">Submit</button>
-                </div>
             </form>
 
         </div>
