3f747f8ab7955dd7304543e1971d0fcc6d61a29d
Ryosuke.K <ryskdevelop@gmail.com>
2023-07-02 18:06:02 +0900

    refacotor: update button

--- src/resources/views/components/templates/button.blade.php
+++ src/resources/views/components/templates/button.blade.php
@@ -8,7 +8,25 @@
     <main class="layout-main">
         <div class="main-container">
 
+            {{-- Button --}}
             <div class="btn-group">
+                <button class="btn">ボタン</button>
+                <button class="btn btn-create">登録</button>
+                <button class="btn btn-store">保存</button>
+                <button class="btn btn-edit">編集</button>
+                <button class="btn btn-update">更新</button>
+                <button class="btn btn-delete">削除</button>
+                <button class="btn btn-search">検索</button>
+                <button class="btn btn-submit">送信</button>
+                <button class="btn btn-info">info</button>
+                <button class="btn btn-index">index</button>
+                <button class="btn btn-back">戻る</button>
+                <button class="btn btn-cancel">キャンセル</button>
+            </div>
+
+            {{-- divタグ Botton --}}
+            <div class="btn-group">
+                <div class="btn">ボタン</div>
                 <div class="btn btn-create">登録</div>
                 <div class="btn btn-store">保存</div>
                 <div class="btn btn-edit">編集</div>
@@ -21,6 +39,37 @@
                 <div class="btn btn-back">戻る</div>
                 <div class="btn btn-cancel">キャンセル</div>
             </div>
+
+            {{-- aタグ Button --}}
+            <div class="btn-group">
+                <a href="" class="btn">ボタン</a>
+                <a href="" class="btn btn-create">登録</a>
+                <a href="" class="btn btn-store">保存</a>
+                <a href="" class="btn btn-edit">編集</a>
+                <a href="" class="btn btn-update">更新</a>
+                <a href="" class="btn btn-delete">削除</a>
+                <a href="" class="btn btn-search">検索</a>
+                <a href="" class="btn btn-submit">送信</a>
+                <a href="" class="btn btn-info">info</a>
+                <a href="" class="btn btn-index">index</a>
+                <a href="" class="btn btn-back">戻る</a>
+                <a href="" class="btn btn-cancel">キャンセル</a>
+            </div>
+
+            {{-- 単体Button --}}
+            <button class="btn">ボタン</button>
+            <button class="btn btn-create">登録</button>
+            <button class="btn btn-store">保存</button>
+            <button class="btn btn-edit">編集</button>
+            <button class="btn btn-update">更新</button>
+            <button class="btn btn-delete">削除</button>
+            <button class="btn btn-search">検索</button>
+            <button class="btn btn-submit">送信</button>
+            <button class="btn btn-info">info</button>
+            <button class="btn btn-index">index</button>
+            <button class="btn btn-back">戻る</button>
+            <button class="btn btn-cancel">キャンセル</button>
+
         </div>
     </main>
 @endsection
