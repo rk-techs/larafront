001b1f9aec5a304e6d131441e49db39578f12b98
Ryosuke.K <ryskdevelop@gmail.com>
2023-07-02 23:12:28 +0900

    update search

--- src/public/js/search.js
+++ src/public/js/search.js
@@ -4,7 +4,7 @@ const toggleSearchHeader = document.getElementById('toggleSearchHeader');
 const toggleExpandSymbol = document.getElementById('toggleExpandSymbol');
 const collapseBody       = document.getElementById('collapseBody');
 
-toggleSearchHeader.addEventListener('click', () => {
+toggleExpandSymbol.addEventListener('click', () => {
   collapseBody.classList.toggle('collapse');
 
   if (toggleExpandSymbol.textContent == 'unfold_less') {
