ab38c9d9c89b20f18f3ac008475a6afad68e2025
Ryosuke.K <ryskdevelop@gmail.com>
2023-07-07 14:07:20 +0900

    refactor: update input block

--- src/resources/views/user/create.blade.php
+++ src/resources/views/user/create.blade.php
@@ -17,47 +17,64 @@
 
             <div class="input-block">
                 <div class="input-body">
-                    <div class="input-col">
-                        <div class="input-row">
+
+                    <div class="row">
+                        <div class="col">
                             <label for="userNameInput" class="form-label">
                                 <span class="label-txt">名前</span>
                                 <span class="required-label">必須</span>
                             </label>
                             <input id="userNameInput" type="text" class="input-field" name="name" placeholder="">
                         </div>
-                        <div class="input-row">
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
                             <label for="userEmailInput" class="form-label">
                                 <span class="label-txt">E-mail</span>
                                 <span class="required-label">必須</span>
                             </label>
                             <input id="userEmailInput" type="email" class="input-field" name="email" placeholder="">
                         </div>
-                        <div class="input-row">
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
                             <label for="userPasswordInput" class="form-label">
                                 <span class="label-txt">Password</span>
                                 <span class="required-label">必須</span>
                             </label>
                             <input id="userPasswordInput" type="password" class="input-field" name="password" placeholder="">
                         </div>
-                        <div class="input-row">
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
                             <label for="userAddressInput" class="form-label">
                                 <span class="label-txt">住所</span>
                             </label>
                             <input id="userAddressInput" type="text" class="input-field" name="address" placeholder="">
                         </div>
-                        <div class="input-row">
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
                             <label for="userMobileNumberInput" class="form-label">
                                 <span class="label-txt">携帯番号</span>
                             </label>
                             <input id="userMobileNumberInput" type="text" class="input-field" name="mobile_number" placeholder="">
                         </div>
-                        <div class="input-row">
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
                             <label for="userRemarksInput" class="form-label">
                                 <span class="label-txt">備考</span>
                             </label>
                             <textarea id="userRemarksInput" rows="5" class="input-field" name="remarks"></textarea>
                         </div>
                     </div>
+
                 </div>
             </div>
 
