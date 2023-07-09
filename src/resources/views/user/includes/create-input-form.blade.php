ced59cd268e4f8e37d3989d2ac6524ecb2deed5e
Ryosuke.K <ryskdevelop@gmail.com>
2023-07-09 16:19:37 +0900

    style: apply formatter for create, edit blade

--- src/resources/views/user/includes/create-input-form.blade.php
+++ src/resources/views/user/includes/create-input-form.blade.php
@@ -9,11 +9,11 @@
                         <span class="label-txt">名前</span>
                         <span class="required-label">必須</span>
                     </label>
-                    <input id="userNameInput" type="text" class="input-field @error('name'){{ 'is-invalid' }}@enderror" name="name" value="{{ old('name') }}">
+                    <input id="userNameInput" type="text"
+                        class="input-field @error('name'){{ 'is-invalid' }}@enderror" name="name"
+                        value="{{ old('name') }}">
                     @error('name')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -24,11 +24,11 @@
                         <span class="label-txt">E-mail</span>
                         <span class="required-label">必須</span>
                     </label>
-                    <input id="userEmailInput" type="email" class="input-field @error('email'){{ 'is-invalid' }}@enderror" name="email" value="{{ old('email') }}">
+                    <input id="userEmailInput" type="email"
+                        class="input-field @error('email'){{ 'is-invalid' }}@enderror" name="email"
+                        value="{{ old('email') }}">
                     @error('email')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -39,11 +39,10 @@
                         <span class="label-txt">Password</span>
                         <span class="required-label">必須</span>
                     </label>
-                    <input id="userPasswordInput" type="password" class="input-field @error('password'){{ 'is-invalid' }}@enderror" name="password">
+                    <input id="userPasswordInput" type="password"
+                        class="input-field @error('password'){{ 'is-invalid' }}@enderror" name="password">
                     @error('password')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -54,11 +53,10 @@
                         <span class="label-txt">Password 再確認</span>
                         <span class="required-label">必須</span>
                     </label>
-                    <input id="userPasswordConfirmationInput" type="password" class="input-field" name="password_confirmation" autocomplete="new-password" placeholder="確認">
+                    <input id="userPasswordConfirmationInput" type="password" class="input-field"
+                        name="password_confirmation" autocomplete="new-password" placeholder="確認">
                     @error('password_confirmation')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -69,16 +67,16 @@
                         <span class="label-txt">権限</span>
                         <span class="required-label">必須</span>
                     </label>
-                    <select id="userPermissionIdInput" class="input-field @error('permission_id'){{ 'is-invalid' }}@enderror" name="permission_id">
+                    <select id="userPermissionIdInput"
+                        class="input-field @error('permission_id'){{ 'is-invalid' }}@enderror" name="permission_id">
                         <option value=""></option>
                         @foreach ($permissions as $permission)
-                        <option value="{{ $permission->id }}" @selected( old('permission_id') == $permission->id)>{{ $permission->display_name }}</option>
+                            <option value="{{ $permission->id }}" @selected(old('permission_id') == $permission->id)>
+                                {{ $permission->display_name }}</option>
                         @endforeach
                     </select>
                     @error('permission_id')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -88,11 +86,11 @@
                     <label for="employeeMobileNumberInput" class="form-label">
                         <span class="label-txt">携帯番号</span>
                     </label>
-                    <input id="employeeMobileNumberInput" type="text" class="input-field @error('mobile_number'){{ 'is-invalid' }}@enderror" name="mobile_number" value="{{ old('mobile_number') }}" >
+                    <input id="employeeMobileNumberInput" type="text"
+                        class="input-field @error('mobile_number'){{ 'is-invalid' }}@enderror" name="mobile_number"
+                        value="{{ old('mobile_number') }}">
                     @error('mobile_number')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -102,7 +100,8 @@
                     <label for="employeeRemarkInput" class="form-label">
                         <span class="label-txt">備考</span>
                     </label>
-                    <textarea id="employeeRemarkInput" rows="5" class="input-field @error('remark'){{ 'is-invalid' }}@enderror" name="remark">{{ old('remark') }}</textarea>
+                    <textarea id="employeeRemarkInput" rows="5" class="input-field @error('remark'){{ 'is-invalid' }}@enderror"
+                        name="remark">{{ old('remark') }}</textarea>
                 </div>
             </div>
 
--- src/resources/views/user/includes/edit-input-form.blade.php
+++ src/resources/views/user/includes/edit-input-form.blade.php
@@ -10,11 +10,11 @@
                         <span class="label-txt">名前</span>
                         <span class="required-label">必須</span>
                     </label>
-                    <input id="userNameInput" type="text" class="input-field @error('name'){{ 'is-invalid' }}@enderror" name="name" value="{{ old('name', $user->name) }}">
+                    <input id="userNameInput" type="text"
+                        class="input-field @error('name'){{ 'is-invalid' }}@enderror" name="name"
+                        value="{{ old('name', $user->name) }}">
                     @error('name')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -25,11 +25,11 @@
                         <span class="label-txt">E-mail</span>
                         <span class="required-label">必須</span>
                     </label>
-                    <input id="userEmailInput" type="email" class="input-field @error('email'){{ 'is-invalid' }}@enderror" name="email" value="{{ old('email', $user->email) }}">
+                    <input id="userEmailInput" type="email"
+                        class="input-field @error('email'){{ 'is-invalid' }}@enderror" name="email"
+                        value="{{ old('email', $user->email) }}">
                     @error('email')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -40,16 +40,16 @@
                         <span class="label-txt">権限</span>
                         <span class="required-label">必須</span>
                     </label>
-                    <select id="userPermissionIdInput" class="input-field @error('permission_id'){{ 'is-invalid' }}@enderror" name="permission_id">
+                    <select id="userPermissionIdInput"
+                        class="input-field @error('permission_id'){{ 'is-invalid' }}@enderror" name="permission_id">
                         <option value=""></option>
                         @foreach ($permissions as $permission)
-                        <option value="{{ $permission->id }}" @selected( old('permission_id', $user->permission->id) == $permission->id)>{{ $permission->display_name }}</option>
+                            <option value="{{ $permission->id }}" @selected(old('permission_id', $user->permission->id) == $permission->id)>
+                                {{ $permission->display_name }}</option>
                         @endforeach
                     </select>
                     @error('permission_id')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -59,11 +59,11 @@
                     <label for="employeeMobileNumberInput" class="form-label">
                         <span class="label-txt">携帯番号</span>
                     </label>
-                    <input id="employeeMobileNumberInput" type="text" class="input-field @error('mobile_number'){{ 'is-invalid' }}@enderror" name="mobile_number" value="{{ old('mobile_number', $user->employee->mobile_number) }}" >
+                    <input id="employeeMobileNumberInput" type="text"
+                        class="input-field @error('mobile_number'){{ 'is-invalid' }}@enderror" name="mobile_number"
+                        value="{{ old('mobile_number', $user->employee->mobile_number) }}">
                     @error('mobile_number')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -73,7 +73,8 @@
                     <label for="employeeRemarkInput" class="form-label">
                         <span class="label-txt">備考</span>
                     </label>
-                    <textarea id="employeeRemarkInput" rows="5" class="input-field @error('remark'){{ 'is-invalid' }}@enderror" name="remark">{{ old('remark', $user->employee->remark) }}</textarea>
+                    <textarea id="employeeRemarkInput" rows="5" class="input-field @error('remark'){{ 'is-invalid' }}@enderror"
+                        name="remark">{{ old('remark', $user->employee->remark) }}</textarea>
                 </div>
             </div>
 
